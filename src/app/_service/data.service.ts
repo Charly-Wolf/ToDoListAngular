75452df0dca759d7b20ec82bdf03e2914ebe4d54
Carlos Paredes <carlos.paredes@gizonline.onmicrosoft.com>
2022-12-02 11:44:02 +0100

    Define GET method in data.servie.ts

--- src/app/_service/data.service.ts
+++ src/app/_service/data.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { ToDo } from '../_interface/todo'; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  private serverUrl = 'http://localhost:3000';
+
+  constructor(
+    private _http: HttpClient
+  ) { }
+
+  // GET
+  public getToDo(): Observable<ToDo[]> {
+    // Header
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+    // Return list of ToDos
+    return this._http.get<ToDo[]>(`${this.serverUrl}/todos`, httpOptions);
+  }
+
 }
