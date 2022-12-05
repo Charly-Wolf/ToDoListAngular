106b4c299d9127597394ae8ca688b66f66ba24cf
Carlos Paredes <carlos.paredes@gizonline.onmicrosoft.com>
2022-12-05 16:07:15 +0100

    modify changeLabel()

--- src/app/_template/template-todo/template-todo.component.ts
+++ src/app/_template/template-todo/template-todo.component.ts
@@ -1,62 +1,80 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Eventping } from 'src/app/_interface/eventping';
-import { ToDo } from '../../_interface/todo'
+import { ToDo } from '../../_interface/todo';
 import { DataService } from 'src/app/_service/data.service';
 
 @Component({
   selector: 'app-template-todo',
   templateUrl: './template-todo.component.html',
-  styleUrls: ['./template-todo.component.sass']
+  styleUrls: ['./template-todo.component.sass'],
 })
 export class TemplateTodoComponent {
-
   @Input() toDo$: ToDo;
   @Output() ping: EventEmitter<any> = new EventEmitter<any>();
 
-  constructor(
-    public _dataService: DataService
-  ) {
+  constructor(public _dataService: DataService) {
     this.toDo$ = {
       id: 1,
       label: 'Beispiel',
       status: false,
-      position: 1
+      position: 1,
     };
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   public changeCheck(event?: any): void {
     this.toDo$.status = !this.toDo$.status;
-    this._dataService.putToDo(this.toDo$).subscribe((data: ToDo) => {
-      const eventObject: Eventping = {
-        label: 'check',
-        object: this.toDo$
-      };
-      this.ping.emit(eventObject);
-    }, error => {
-      console.log(`%cERROR: ${error.message}`, `color: red; font-size: 12px;`);
-    });
+    this._dataService.putToDo(this.toDo$).subscribe(
+      (data: ToDo) => {
+        const eventObject: Eventping = {
+          label: 'check',
+          object: this.toDo$,
+        };
+        this.ping.emit(eventObject);
+      },
+      (error) => {
+        console.log(
+          `%cERROR: ${error.message}`,
+          `color: red; font-size: 12px;`
+        );
+      }
+    );
   }
 
   public changeLabel(event?: any): void {
-    const eventObject: Eventping = {
-      label: 'label',
-      object: this.toDo$
-    };
-    this.ping.emit(eventObject);
+    this._dataService.putToDo(this.toDo$).subscribe(
+      (data: ToDo) => {
+        const eventObject: Eventping = {
+          label: 'label',
+          object: this.toDo$,
+        };
+        this.ping.emit(eventObject);
+      },
+      (error) => {
+        console.log(
+          `%cERROR: ${error.message}`,
+          `color: red; font-size: 12px;`
+        );
+      }
+    );
   }
 
   public deleteToDo(event?: any): void {
-    this._dataService.deleteToDo(this.toDo$).subscribe((data: ToDo) => {
-      const eventObject: Eventping = {
-        label: 'delete',
-        object: this.toDo$
-      };
-      this.ping.emit(eventObject);
-    }, error => {
-      console.log(`%cERROR: ${error.message}`, `color: red; font-size: 12px;`);
-    })
+    this._dataService.deleteToDo(this.toDo$).subscribe(
+      (data: ToDo) => {
+        const eventObject: Eventping = {
+          label: 'delete',
+          object: this.toDo$,
+        };
+        this.ping.emit(eventObject);
+      },
+      (error) => {
+        console.log(
+          `%cERROR: ${error.message}`,
+          `color: red; font-size: 12px;`
+        );
+      }
+    );
   }
 }
